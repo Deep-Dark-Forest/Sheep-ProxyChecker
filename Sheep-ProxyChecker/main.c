6d9037ce265c646647cbb2349ccc88232907594c
深黑森林 <131975043+Deep-Dark-Forest@users.noreply.github.com>
2024-06-01 22:59:40 +0800

    更新 main.c

--- Sheep-ProxyChecker/main.c
+++ Sheep-ProxyChecker/main.c
@@ -3,23 +3,24 @@
 #include <string.h>
 #include <windows.h>
 
+constexpr auto MAX_SITE_LENGTH = 50;
+constexpr auto MAX_PROXYTYPE_LENGTH = 10;
 // 定义配置和代理列表文件的名称
-#define CONFIG_FILE "config.txt"
-#define PROXY_LIST_FILE "proxylist.txt"
+constexpr auto CONFIG_FILE = "config.txt";
+constexpr auto PROXY_LIST_FILE = "proxylist.txt";
 
 // 用于存储配置的结构体
 struct Config {
     int timeout;
-    char testsite[50];
-    char proxytype[10];
+    char testsite[MAX_SITE_LENGTH];
+    char proxytype[MAX_PROXYTYPE_LENGTH];
 };
 
-// 函数声明
 void createConfig();
 void checkProxies();
 void setColor(int color);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     if (argc < 2) {
         printf("使用方法: %s <命令>\n命令:\n start - 生成配置文件\n check - 检查代理\n", argv[0]);
         return 1;
@@ -27,9 +28,11 @@ int main(int argc, char *argv[]) {
 
     if (strcmp(argv[1], "start") == 0) {
         createConfig();
-    } else if (strcmp(argv[1], "check") == 0) {
+    }
+    else if (strcmp(argv[1], "check") == 0) {
         checkProxies();
-    } else {
+    }
+    else {
         printf("未知命令: %s\n", argv[1]);
     }
 
@@ -38,11 +41,11 @@ int main(int argc, char *argv[]) {
 
 // 创建配置文件和代理列表文件
 void createConfig() {
-    FILE *configFile;
-    FILE *proxyListFile;
+    FILE* configFile;
+    FILE* proxyListFile;
 
     // 创建config.txt
-    configFile = fopen(CONFIG_FILE, "w");
+    fopen_s(&configFile, CONFIG_FILE, "w");
     if (configFile == NULL) {
         perror("无法创建配置文件");
         exit(1);
@@ -53,7 +56,7 @@ void createConfig() {
     fclose(configFile);
 
     // 创建proxylist.txt
-    proxyListFile = fopen(PROXY_LIST_FILE, "w");
+    fopen_s(&proxyListFile, PROXY_LIST_FILE, "w");
     if (proxyListFile == NULL) {
         perror("无法创建代理列表文件");
         exit(1);
@@ -65,45 +68,48 @@ void createConfig() {
 
 // 检查代理列表中的代理
 void checkProxies() {
-    struct Config config;
-    FILE *configFile;
-    FILE *proxyListFile;
-    char proxy[50];
+    struct Config config {};
+    FILE* configFile;
+    FILE* proxyListFile;
+    char proxy[MAX_SITE_LENGTH];
     char command[100];
     int result;
 
     // 读取配置文件
-    configFile = fopen(CONFIG_FILE, "r");
-    if (configFile == NULL) {
+    
+    if (fopen_s(&configFile, CONFIG_FILE, "r") != 0) {
         perror("无法打开配置文件");
         exit(1);
     }
-    fscanf(configFile, "timeout = %dms\n", &config.timeout);
-    fscanf(configFile, "testsite = %s\n", config.testsite);
-    fscanf(configFile, "proxytype = %s\n", config.proxytype);
+    fscanf_s(configFile, "timeout = %dms\n", &config.timeout);
+    fscanf_s(configFile, "testsite = %s\n", &config.testsite, MAX_SITE_LENGTH);
+    fscanf_s(configFile, "proxytype = %s\n", &config.proxytype, MAX_PROXYTYPE_LENGTH);
     fclose(configFile);
 
     // 打开代理列表文件
-    proxyListFile = fopen(PROXY_LIST_FILE, "r");
-    if (proxyListFile == NULL) {
+    if (fopen_s(&proxyListFile, PROXY_LIST_FILE, "r") != 0) {
         perror("无法打开代理列表文件");
         exit(1);
     }
 
     // 逐行读取代理并检查
-    while (fgets(proxy, 50, proxyListFile) != NULL) {
+    while (fgets(proxy, MAX_SITE_LENGTH, proxyListFile) != NULL) {
         // 构建ping命令
-        snprintf(command, sizeof(command), "ping -n 1 -w %d %s", config.timeout, config.testsite);
+        snprintf(command, sizeof(command), "ping -n 1 -w %d %s", config.timeout, proxy);
         // 执行ping命令
         result = system(command);
         // 根据结果设置颜色
-        if (result == 0) {
+        switch (result) {
+        case 0:
             setColor(2); // 绿色
-        } else if (result == 1) {
+            break;
+        case 1:
             setColor(14); // 黄色
-        } else if (result == 2) {
+            break;
+        case 2:
             setColor(6); // 橙色
-        } else {
+            break;
+        default:
             setColor(4); // 红色
         }
         printf("%s\n", proxy);
